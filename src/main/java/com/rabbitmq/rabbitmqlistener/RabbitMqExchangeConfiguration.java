9f237ffb1d693d4bc71008b537ae4c6a08fd56bd
eltoman <eltomds@usp.br>
2020-08-19 23:28:13 -0300

    Add rabbit mq exchange configuration

---
+++ src/main/java/com/rabbitmq/rabbitmqlistener/RabbitMqExchangeConfiguration.java
@@ -0,0 +1,33 @@
+package com.rabbitmq.rabbitmqlistener;
+
+import org.springframework.amqp.core.Exchange;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqExchangeConfiguration {
+
+    @Bean
+    Exchange exampleExchange(){
+        return new TopicExchange("ExampleExchange");
+    }
+
+    @Bean
+    Exchange example2ndExchange(){
+        return ExchangeBuilder.directExchange("Example2ndExchange")
+                .autoDelete()
+                .internal()
+                .build();
+    }
+
+    @Bean
+    Exchange newExchange(){
+        return ExchangeBuilder.topicExchange("TopicTestExchange")
+                .autoDelete()
+                .durable(true)
+                .internal()
+                .build();
+    }
+}
